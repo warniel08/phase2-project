42d575df7feed2bf0a1bc91bb9b624c180634aa2
Warner Nielsen <bwn888@gmail.com>
2017-01-20 15:24:28 -0800

    Add environment stuff for dotenv, fix the weather method in the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'open-uri'
 require 'json'
 
 class Post < ActiveRecord::Base
@@ -13,9 +14,19 @@ class Post < ActiveRecord::Base
     #need to write the weather api stuff here
     state = "WA"
     city = "Seattle"
-    url = "http://api.wunderground.com/api/ENV[WEATHER_UNDERGROUND_API_KEY]/conditions/q/#{state}/#{city}.json"
+    url = "http://api.wunderground.com/api/#{ENV["WEATHER_UNDERGROUND_API_KEY"]}/geolookup/conditions/q/#{state}/#{city}.json"
     response = HTTParty.get(url)
     data = JSON.parse(response.body)
-    p data
+    location = data['location']['city']
+    temp_f = data['current_observation']['temp_f']
+    p "Current temperature in #{location} is: #{temp_f}"
+
+    # open(url) do |f|
+    #   json_string = f.read
+    #   parsed_json = JSON.parse(json_string)
+    #   location = parsed_json['location']['city']
+    #   temp_f = parsed_json['current_observation']['temp_f']
+    #   print "Current temperature in #{location} is: #{temp_f}\n"
+    # end
   end
 end
