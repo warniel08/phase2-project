ae10191743a8227253ab31eb8a2a5f3ab74a545f
Warner Nielsen <bwn888@gmail.com>
2017-01-19 16:26:09 -0800

    Remove some gitkeep files, change navigation link helpers a bit, update the view layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :posts
+  include BCrypt
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+
+    if user.password == password
+      user
+    else
+      nil
+    end
+  end
 end
